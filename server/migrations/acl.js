24b2309d632a57a96b7da07dd397e2390c3b376a
Truong Duong <duongtd@sololab.net>
2015-12-04 10:04:01 +0700

    ignore all the local environtments

---
+++ server/migrations/acl.js
@@ -0,0 +1,9 @@
+// Migrate loopback builtin User & ACL system
+var server = require('../server');
+var ds = server.dataSources.mysql;
+var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+ds.automigrate(lbTables, function(er) {
+  if (er) throw er;
+  console.log('Looback tables [' + lbTables + '] created in ', ds.adapter.name);
+  ds.disconnect();
+});
