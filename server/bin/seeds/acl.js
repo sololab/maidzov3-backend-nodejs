f36a495ffb4c13af18301e7b330cfed60ebe1d60
Tuan Lai <laituan245@kaist.ac.kr>
2015-12-04 10:30:07 +0700

    Added code to create the admin role and the member role

--- server/bin/seeds/acl.js
+++ server/bin/seeds/acl.js
@@ -1,1 +1,21 @@
 // Seed for Users and Roles
+var app = require('../../server');
+var User = app.models.user;
+var Role = app.models.Role;
+var RoleMapping = app.models.RoleMapping;
+
+//create the admin role
+Role.create({
+  name: 'admin'
+}, function(err, role) {
+  if (err) throw err;
+  console.log('Created role:', role);
+});
+
+//create the member role
+Role.create({
+  name: 'member'
+}, function(err, role) {
+  if (err) throw err;
+  console.log('Created role:', role);
+});
