7b35313a054928185fef6261907bf06d81b64b6f
Tuan Lai <laituan245@kaist.ac.kr>
2015-12-08 11:12:46 +0700

    Added more timestamp mixins

--- common/mixins/timestamp.js
+++ common/mixins/timestamp.js
@@ -1,4 +1,4 @@
 module.exports = function(Model, options) {
   Model.defineProperty('created', {type: Date, default: '$now'});
-  Model.defineProperty('modified', {type: Date, default: '$now'});
+  Model.defineProperty('lastUpdated', {type: Date, default: '$now'});
 };
