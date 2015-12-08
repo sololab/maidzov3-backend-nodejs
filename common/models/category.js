2a6d88a71efb37b0e6a13ff1f63e3033f0e00e28
Truong Duong <duongtd@sololab.net>
2015-12-08 14:32:21 +0700

    refactory model to it's module

--- common/models/category.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(Category) {
-  // Hide some methods
-  Category.disableRemoteMethod('createChangeStream', true);
-
-  // Model constraint
-  Category.validatesUniquenessOf('slug', {message: 'slug is not unique'});
-};
--- common/models/post.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(Post) {
-  // Hide some methods
-  Post.disableRemoteMethod('createChangeStream', true);
-
-  // Model constraint
-  Post.validatesUniquenessOf('slug', {message: 'slug is not unique'});
-};
