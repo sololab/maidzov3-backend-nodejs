cb24b860584cd826b58373b2a1c7253873eac099
Truong Duong <duongtd@sololab.net>
2015-12-08 11:04:21 +0700

    remove self timestamp mixin to use loopback-ds-timestamp-mixin module

--- common/mixins/timestamp.js
+++
@@ -1,4 +0,0 @@
-module.exports = function(Model, options) {
-  Model.defineProperty('created', {type: Date, default: '$now'});
-  Model.defineProperty('modified', {type: Date, default: '$now'});
-};
